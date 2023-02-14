07615d2936e8e86a26946154ea1014d715995bc9
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-02-14 17:43:48 -0500

    fixed

--- src/mdsvexlayout.svelte
+++ src/mdsvexlayout.svelte
@@ -1,6 +1,6 @@
 <!-- https://tailwindcss.com/docs/typography-plugin#element-modifiers -->
 <article
-	class="container prose:xl mx-auto mb-12 hover:prose-a:text-blue-300
+	class="container prose mx-auto mb-12 hover:prose-a:text-blue-300
 prose-ul:list-disc
 dark:prose-invert
 "
