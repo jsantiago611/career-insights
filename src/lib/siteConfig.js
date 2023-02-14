adb495eac1118101c86d9f2de49ece6faa757168
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-02-14 10:34:42 -0500

    fixes typo

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -2,7 +2,7 @@ export const SITE_URL = 'gospelgardenv2.netlify.app';
 export const APPROVED_POSTERS_GH_USERNAME = ['jsantiago611'];
 export const GH_USER_REPO = 'jsantiago611/atomicgospel'; // used for pulling github issues and offering comments
 export const SITE_TITLE = 'Gospel Garden';
-export const SITE_DESCRIPTION = "A Digital Garden about Christinity and the Gospel";
+export const SITE_DESCRIPTION = "A Digital Garden about Christianity and the Gospel";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'jderekjames';
