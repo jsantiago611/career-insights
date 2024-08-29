85bcec5b04df1bfca3aa7736de5c7aa840ba2dcf
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-08-29 14:42:46 -0400

    Update FeatureCard.svelte

--- src/components/FeatureCard.svelte
+++ src/components/FeatureCard.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <a
-	class="w-full transform rounded-xl bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 p-1 transition-all hover:scale-[1.01] md:w-1/3"
+	class="w-full transform rounded-sm bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 p-1 transition-all hover:scale-[1.01] md:w-1/3"
 	{href}
-	><div class="flex h-full flex-col justify-between rounded-lg bg-white p-4 dark:bg-gray-900">
+	><div class="flex h-full flex-col justify-between rounded-sm bg-white p-4 dark:bg-gray-900">
 		<div class="flex flex-col justify-between md:flex-row">
 			<h4
 				class="mb-6 w-full text-lg font-medium tracking-tight text-gray-900 dark:text-gray-100 sm:mb-10 md:text-lg"
