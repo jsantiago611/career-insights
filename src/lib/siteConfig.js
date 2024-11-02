44b51394e2f8ee2c946f0edb509c360dce09cf49
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-11-01 22:16:02 -0500

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,13 +1,13 @@
 export const SITE_URL = 'seriousmanagersonly.netlify.app';
 export const APPROVED_POSTERS_GH_USERNAME = ['jsantiago611'];
 export const GH_USER_REPO = 'jsantiago611/career-insights'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'Gospel Garden';
-export const SITE_DESCRIPTION = "A Digital Garden about Christianity and the Gospel";
+export const SITE_TITLE = 'QA Leadership Handbook';
+export const SITE_DESCRIPTION = "A Digital handbook on leadership, managerment, amd software testing";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'jderekjames';
 export const MY_YOUTUBE = '';
-export const POST_CATEGORIES = ['ReasonWhy', 'LifeLesson', 'SermonSummary']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const POST_CATEGORIES = ['Notes']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
 // auto generated variables
